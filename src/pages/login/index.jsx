a0c9a2d501dfbca74c2d693209f8f45b17c538f8
Marcelo Siriaki <msiriaki@gmail.com>
2024-01-22 23:38:47 -0300

    select mode form editing

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import useUser from "../../hooks/useUser";
 
 const Login = () => {
-
+    const [mode, setMode] = useState(false);
     const { gameMode, setGameMode } = useUser();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMode(false);
+    }
+
+    const handleSelectDifficult = (e) => {
+        setGameMode({ ...gameMode, difficult: e.target.value });
+    }
+
+    const handleSelectDisplay = (e) => {
+        setGameMode({ ...gameMode, showAll: e.target.value });
+    }
 
     return (
         <div className="login-container">
-            <div className="login-title-container">
-                <h1>Start Game</h1>
-                <h1>Mode</h1>
-            </div>
+            {mode ?
+                (<div className="login-title-container">
+                    <form
+                        className="login-form-mode"
+                        onSubmit={handleSubmit}
+                    >
+                        <label>
+                            Select difficult
+                            <select
+                                onChange={(e) => handleSelectDifficult(e)}
+                                value={gameMode.difficult}
+                            >
+                                <option value="easy">Easy</option>
+                                <option value="medium">Medium</option>
+                                <option value="hard">Hard</option>
+                            </select>
+                        </label>
+                        <label>
+                            Select display option
+                            <select
+                                onChange={handleSelectDisplay}
+                                value={gameMode.showAll}
+                            >
+                                <option value={true}>Show all cards before start</option>
+                                <option value={false}>Don't show any card</option>
+                            </select>
+                        </label>
+
+                        <button type="submit">Ready</button>
+                    </form>
+                </div>)
+                :
+                (<div className="login-title-container">
+                    <h1>Start Game</h1>
+                    <h1 onClick={() => setMode(true)}>Mode</h1>
+                </div>)}
         </div>
     )
 }
